ce65a48ebb05dd97729e578d7113fccb5159e891
Ebrahim <Ebrahim.elhabashy354@gmail.com>
2022-05-04 09:31:10 +0200

    fix package.json

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 const { PORT, SOCKET_PORT } = require('./utils/config');
 
-const io = require('socket.io')(SOCKET_PORT, {
-	cors: {
-		origin: ['http://localhost:3000'],
-	},
-});
-
-io.on('connection', socket => {
-	console.log('someone connected: ', socket.id);
-});
-
 const App = require('./App');
 const http = require('http');
 const server = http.createServer(App);
